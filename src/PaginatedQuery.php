76dce727169ad9bbfa269080e1d3120501e79d3a
christ974 <garrechrist@gmail.com>
2019-09-30 13:05:38 +0400

    Ajout des méthodes previousLink & nextLink PaginatedQuery

--- src/PaginatedQuery.php
+++ src/PaginatedQuery.php
@@ -10,6 +10,7 @@ class PaginatedQuery
     private $classMapping;
     private $pdo;
     private $perPage;
+    private $count;
 
     public function __construct(
         string $query,
@@ -28,16 +29,8 @@ class PaginatedQuery
 
     public function getItems(): array
     {
-        $page = $_GET['page'] ?? 1;
-            if(!filter_var($page, FILTER_VALIDATE_INT))
-                {
-                    throw new Exception('Numéro de page invalide');
-                }
-        $currentPage = (int)$page;  
-        $count = (int) $this->pdo
-            -> query($this->queryCount)
-            ->fetch(PDO::FETCH_NUM)[0];
-        $pages = ceil($count / $this->perPage);
+        $currentPage = $this->getCurrentPage();
+        $pages = $this->getPages();
         if($currentPage > $pages){
             throw new Exception('Page inexistante');
         }
@@ -49,6 +42,46 @@ class PaginatedQuery
 
     }
 
-    
+    public function previousLink(string $link): ?string
+    {
+        $currentPage = $this->getCurrentPage();
+        if($currentPage <= 1) return null;
+        if ($currentPage > 2) $link .= "?page="  .  ($currentPage - 1);
+        return <<<HTML
+            <a href="{$link}" class="btn btn-info">&laquo; Page précédente</a>
+HTML;
+    }
 
+    public function nextLink(string $link): ?string
+    {
+        $currentPage = $this->getCurrentPage();
+        $pages = $this->getPages();
+        if($currentPage >= $pages) return null;
+        $link .= "?page=" . ($currentPage + 1);
+        return <<<HTML
+            <a href="{$link}" class="btn btn-info">Page suivante &raquo;</a>
+HTML;
+    }
+
+    //création d'une méthode privée pr récupérer la page courante
+    private function getCurrentPage(): int
+    {
+        $page = $_GET['page'] ?? 1;
+            if(!filter_var($page, FILTER_VALIDATE_INT))
+                {
+                    throw new Exception('Numéro de page invalide');
+                }
+        return $currentPage = (int)$page;
+    }
+    //création d'une méthode privée pr connaitre le nbre de pages
+    private function getPages()
+    {
+        if($this->count === null){
+            $this->count = (int) $this->pdo
+                -> query($this->queryCount)
+                ->fetch(PDO::FETCH_NUM)[0];
+        }
+        
+        return ceil($this->count / $this->perPage);
+    }
 }
--- views/category/show.php
+++ views/category/show.php
@@ -40,7 +40,7 @@ $paginatedQuery = new PaginatedQuery(
 );
 /**@var Post[] */
 $posts = $paginatedQuery->getItems();
-dd($posts);
+//dd($posts);
 /*TOUT CE CODE A ETE INTRODUIT DS LA CLASSE PAGINATEDQUERY!!!*/
 /*récupération de la page courante
 $page = $_GET['page'] ?? 1;
@@ -92,17 +92,9 @@ $link = $router->url('category', ['id' => $category->getId(), 'slug' => $categor
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-
-    <?php
-        $l = $link;
-        if($currentPage > 2) $l = $link . '?page=' . ($currentPage - 1);   
-    ?>
-        <a href="<?= $l ?> "class="btn btn-info" >&laquo; Page précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-        <a href="<?= $link ?>?page=<?= $currentPage + 1 ?> "class="btn btn-info ml-auto" > Page suivante &raquo;</a>
-    <?php endif ?>
+   <?= $paginatedQuery->previousLink($link) ?>
+   <?= $paginatedQuery->nextLink($link) ?>
+    
 </div>
 
 
